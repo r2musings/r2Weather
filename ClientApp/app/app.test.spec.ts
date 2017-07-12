2dfc503313bb8606f00deaf8421530fac80d00ca
r2musings <r2@r2musings.com>
2017-07-12 10:40:44 -0400

    Added sample server and client tests.

---
+++ ClientApp/app/app.test.spec.ts
@@ -0,0 +1,13 @@
+﻿import { } from 'jasmine';
+
+describe('example test 1', () => {
+    it('true is true', () => expect(true).toBe(true));
+});
+
+describe('example test 2', () => {
+    it('false is false', () => expect(false).toBe(false));
+});
+
+describe('example test 3', () => {
+    it('99 is 99', () => expect(99).toBe(99));
+});
